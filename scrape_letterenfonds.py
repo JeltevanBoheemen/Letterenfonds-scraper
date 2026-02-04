36a98937d1da7f5bb0577196623feb7ddf0b3d3e
Jelte van Boheemen <j.vanboheemen@uu.nl>
2026-02-04 14:31:52 +0100

    Make code generalisable

--- scrape_letterenfonds.py
+++ scrape_letterenfonds.py
@@ -1,4 +1,4 @@
-import csv
+from typing import Optional
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -6,20 +6,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from csv import DictWriter
+from urllib.parse import urlencode
 
 READY_XPATH = '/html/body/div/main/div/div/div[2]/div[2]/div[1]/div/span/span[2]'
 
 TIMEOUT = 5
-TITLE = 'letterenfonds_josefien.csv'
+TITLE = 'letterenfonds_output.csv'
 
 BASE_URL = 'https://www.letterenfonds.nl/en/translation-database?'
 
 
 class LetterenFondsScraper:
-    def base_param(param_name):
+    def base_param(self, param_name: str) -> str:
         return (
             f'replica_sa_author_translations_english[refinementList][{param_name}][0]'
         )
+
+    def range_param(self, param_name: str) -> str:
+        return f'replica_sa_author_translations_english[range][{param_name}]'
+
+    def page_param(self, page_nr: int) -> str:
+        f'replica_sa_author_translations_english[page][{page_nr}]'
 
     def __init__(
         self,
@@ -28,7 +35,7 @@ class LetterenFondsScraper:
         year_max: int,
         genre: str = 'Fiction',
         publication_status: str = 'Published',
-        n_pages: int = -1,
+        n_pages: int = 1,
     ):
         self.genre = genre
         self.language = language
@@ -37,165 +44,142 @@ class LetterenFondsScraper:
         self.publication_status = publication_status
         self.n_pages = n_pages
 
-        self.constant_params = {}
+        self.constant_params = self.build_constant_params()
 
     def param_part(self, param_name: str, param_value):
-        return self.base_param(param_name) + f'={param_value}'
+        return self.base_param(param_name), param_value
 
     def page_params(self, page_num: int = -1):
         pass
 
+    def build_constant_params(self):
+        return {
+            self.base_param('translation_genres'): self.genre,
+            self.base_param('translation_languages'): self.language,
+            self.base_param('translation_publication_status'): self.publication_status,
+            self.range_param('translation_years'): f'{self.year_min}:{self.year_max}',
+        }
 
-def generate_urls_french():
-    first_page = 'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Frans&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1900%3A2000'
+    def single_page_params(self, page_nr: Optional[int] = None):
+        if page_nr is None:
+            return self.constant_params
+        else:
+            full_params = self.constant_params.update(
+                {'replica_sa_author_translations_english[page]': str(page_nr)}
+            )
+            return full_params
 
-    def page_url(x):
-        return (
-            f'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Frans&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1900%3A2000&replica_sa_author_translations_english%5Bpage%5D={x}'
-        )
+    def single_page_url(self, page_nr: Optional[int] = None):
+        params = self.single_page_params(page_nr)
+        return BASE_URL + urlencode(params)
 
-    return [first_page] + [page_url(i) for i in range(2, 10)]
+    def generate_urls(self):
+        urls = [self.single_page_url()]  # first page
+        if self.n_pages > 1:
+            for i in range(2, self.n_pages + 1):
+                urls.append(self.single_page_url(i))
+        return urls
 
-
-def generate_urls_german_1950():
-    first_page = 'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1900%3A1950'
-
-    def page_url(x):
-        return (
-            f'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1900%3A1950&replica_sa_author_translations_english%5Bpage%5D={x}'
-        )
-
-    return [first_page] + [page_url(i) for i in range(2, 7)]
-
-
-def generate_urls_german_2000():
-    first_page = 'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1951%3A2000'
-
-    def page_url(x):
-        return (
-            f'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=1951%3A2000&replica_sa_author_translations_english%5Bpage%5D={x}'
-        )
-
-    return [first_page] + [page_url(i) for i in range(2, 21)]
-
-
-def generate_urls_josefien():
-    first_page = 'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=2010%3A2023'
-
-    def page_url(x):
-        return (
-            f'https://www.letterenfonds.nl/en/translation-database?replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_genres%5D%5B0%5D=Fiction&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_languages%5D%5B0%5D=Duits&replica_sa_author_translations_english%5BrefinementList%5D%5Btranslation_publication_status%5D%5B0%5D=Published&replica_sa_author_translations_english%5Brange%5D%5Btranslation_years%5D=2010%3A2023&replica_sa_author_translations_english%5Bpage%5D={x}'
-        )
-    return [first_page] + [page_url(i) for i in range(2, 4)]
-
-
-def retrieve_page(url):
-    driver = webdriver.Firefox()
-    driver.get(url)
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, READY_XPATH))
-        WebDriverWait(driver, TIMEOUT).until(element_present)
-        page = driver.page_source
-        driver.close()
-        driver.quit()
-        return BeautifulSoup(page, 'html.parser')
-    except TimeoutException:
-        print('Timed out waiting for page to load')
-        return None
-
-
-def get_entries(page_soup):
-    return page_soup.find_all(
-        'span', {'class': ['col-span-full', 'text-color-text-base']}
-    )
-
-
-def clean_text(x):
-    return x.get_text().strip()
-
-
-def get_dataid(soup, data_id, leftstrip=None, replaces=[]):
-    try:
-        result = clean_text(soup.find('span', {'data-id': data_id}))
-        if leftstrip:
-            result = result.lstrip(leftstrip)
-        return result
-    except Exception as e:
-        print(e)
-        return ''
-
-
-def parse_entry(entry_soup):
-    children = list(entry_soup.children)
-
-    result = {}
-    result['author'] = clean_text(children[0]).rstrip('.').replace('  ', '')
-    result['book-title'] = get_dataid(entry_soup, 'book-title').strip('.')
-    result['translation-languages'] = get_dataid(entry_soup, 'translation-languages')
-    result['original-languages'] = get_dataid(entry_soup, 'original-languages').replace(
-        '/ trans. from  ', ''
-    )
-    result['translators'] = (
-        get_dataid(entry_soup, 'translators').replace('by ', '').rstrip('.')
-    )
-    result['translation-publisher'] = get_dataid(entry_soup, 'translation-publisher')
-    result['translation-year'] = int(
-        get_dataid(entry_soup, 'translation-years', leftstrip=', ')
-    )
-    result['translation-genres'] = get_dataid(entry_soup, 'translation-genres')
-    result['origin-title'] = clean_text(
-        entry_soup.find('span', {'data-id': 'origin-title'}).find('i')
-    )
-    result['origin-publisher'] = get_dataid(entry_soup, 'translation-languages')
-    result['origin-year'] = get_dataid(entry_soup, 'origin-years', leftstrip=', ')
-    result['translation-locations'] = get_dataid(entry_soup, 'translation-locations')
-
-    return result
-
-
-def parse_page(url):
-    page = retrieve_page(url)
-    entries = get_entries(page)
-    results = [parse_entry(e) for e in entries]
-    return results
-
-
-def parse_all_pages():
-    urls = generate_urls_josefien()
-    counter = 1
-    results = []
-    for url in urls:
+    def retrieve_page(self, url):
+        driver = webdriver.Firefox()
+        driver.get(url)
         try:
-            results += parse_page(url)
-            print(f'parsed page {counter}')
-            counter += 1
-        except Exception as e:
-            print(f'failed to parse page {counter}', e)
-            break
-    return results
+            element_present = EC.presence_of_element_located((By.XPATH, READY_XPATH))
+            WebDriverWait(driver, TIMEOUT).until(element_present)
+            page = driver.page_source
+            driver.close()
+            driver.quit()
+            return BeautifulSoup(page, 'html.parser')
+        except TimeoutException:
+            print('Timed out waiting for page to load')
+            return None
 
+    def get_entries(self, page_soup):
+        return page_soup.find_all(
+            'span', {'class': ['col-span-full', 'text-color-text-base']}
+        )
 
-def generate_results():
-    results = parse_all_pages()
-    with open(TITLE, 'w', encoding='utf-16') as f:
-        writer = DictWriter(f, fieldnames=results[0].keys(), delimiter=';')
-        writer.writeheader()
-        for r in results:
-            writer.writerow(r)
+    def clean_text(self, x):
+        return x.get_text().strip()
 
+    def get_dataid(self, soup, data_id, leftstrip=None, replaces=[]):
+        try:
+            result = self.clean_text(soup.find('span', {'data-id': data_id}))
+            if leftstrip:
+                result = result.lstrip(leftstrip)
+            return result
+        except Exception:
+            return ''
 
-def convert_to_utf16(filename):
-    with open(filename, 'r', encoding='utf-8') as f_in:
-        with open(
-            filename.replace('.csv', '_utf16.csv'), 'w', encoding='utf-16', newline=''
-        ) as f_out:
-            writer = csv.writer(f_out, dialect='excel')
-            for row in csv.reader(f_in):
-                writer.writerow(row)
+    def parse_entry(self, entry_soup):
+        children = list(entry_soup.children)
+
+        result = {}
+        result['author'] = self.clean_text(children[0]).rstrip('.').replace('  ', '')
+        result['book-title'] = self.get_dataid(entry_soup, 'book-title').strip('.')
+        result['translation-languages'] = self.get_dataid(
+            entry_soup, 'translation-languages'
+        )
+        result['original-languages'] = self.get_dataid(
+            entry_soup, 'original-languages'
+        ).replace('/ trans. from  ', '')
+        result['translators'] = (
+            self.get_dataid(entry_soup, 'translators').replace('by ', '').rstrip('.')
+        )
+        result['translation-publisher'] = self.get_dataid(
+            entry_soup, 'translation-publisher'
+        )
+        result['translation-year'] = int(
+            self.get_dataid(entry_soup, 'translation-years', leftstrip=', ')
+        )
+        result['translation-genres'] = self.get_dataid(entry_soup, 'translation-genres')
+        result['origin-title'] = self.clean_text(
+            entry_soup.find('span', {'data-id': 'origin-title'}).find('i')
+        )
+        result['origin-publisher'] = self.get_dataid(
+            entry_soup, 'translation-languages'
+        )
+        result['origin-year'] = self.get_dataid(
+            entry_soup, 'origin-years', leftstrip=', '
+        )
+        result['translation-locations'] = self.get_dataid(
+            entry_soup, 'translation-locations'
+        )
+
+        return result
+
+    def parse_page(self, url):
+        page = self.retrieve_page(url)
+        entries = self.get_entries(page)
+        results = [self.parse_entry(e) for e in entries]
+        return results
+
+    def parse_all_pages(self):
+        urls = self.generate_urls()
+        counter = 1
+        results = []
+        for url in urls:
+            try:
+                results += self.parse_page(url)
+                print(f'parsed page {counter}')
+                counter += 1
+            except Exception as e:
+                print(f'failed to parse page {counter}', e)
+                break
+        return results
+
+    def generate_results(self):
+        results = self.parse_all_pages()
+        with open(TITLE, 'w', encoding='utf-16') as f:
+            writer = DictWriter(f, fieldnames=results[0].keys(), delimiter=';')
+            writer.writeheader()
+            for r in results:
+                writer.writerow(r)
 
 
 if __name__ == '__main__':
-    generate_results()
-    # parse_page(generate_urls()[0])
-    # convert_to_utf16('generated_datasets/dutch_german_all.csv')
-    # convert_to_utf16('generated_datasets/dutch_french.csv')
+    scraper = LetterenFondsScraper(
+        language='Duits', year_min=1900, year_max=2000, genre='Fiction'
+    )
+
+    scraper.generate_results()
